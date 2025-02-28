80514423a7ccd2fb8c2eb8ce318d97436b9f83b7
leannsttar <105887401+leannsttar@users.noreply.github.com>
2025-02-28 14:11:18 -0600

    Cambios del desafío práctico

--- src/Controllers/RentalController.java
+++ src/Controllers/RentalController.java
@@ -14,6 +14,32 @@ public class RentalController {
     private int totalDays;
     private int extraDays;
 
+    //AÑADIENDO SETTERS PARA ASIGNAR VALORES A LOS ATRIBUTOS PRIVADOS
+    //setter de exitDate
+    public void setExitDate (String date) {
+        this.exitDate = LocalDate.parse(date);
+    }
+
+    //setter de estimateReturnDate
+    public void setEstimateReturnDate (String date) {
+        this.estimateReturnDate = LocalDate.parse(date);
+    }
+
+    //setter de returnDate
+    public void setReturnDate (String date) {
+        this.returnDate = LocalDate.parse(date);
+    }
+
+    //AÑADIENDO GETTERS PARA PODER OBTENER LOS ATRIBUTOS PRIVADOS
+    //getter de totaldays
+    public int getTotalDays() {
+        return totalDays;
+    }
+
+    public LocalDate getExitDay() {
+        return exitDate;
+    }
+
     public void calculateTotalDays() {
         this.totalDays = Math.toIntExact(Duration.between(this.exitDate.atStartOfDay(), this.estimateReturnDate.atStartOfDay()).toDays());
     }
@@ -25,23 +51,41 @@ public class RentalController {
     //Funcion para calcular el costo total del vehiculo
     public double calculateBillingTotal(double pricePerDay){
         double total = pricePerDay;
+
         
         //Calcular el costo extra por dias de retraso
-        if(this.extraDays > 1){
-            total += 1.00;
-            System.out.println("Extra day 1 ");
-        }
-        if(this.extraDays > 3){
-            total += 5.00;
-            System.out.println("Extra day 2 ");
-        }
-        if(this.extraDays > 5){
-            total += 10.00;
-            System.out.println("Extra day 3 ");
-        }
+//        if(this.extraDays > 1){
+//            total += 1.00;
+//            System.out.println("Extra day 1 ");
+//        }
+//        else if(this.extraDays > 3){
+//            total += 5.00;
+//            System.out.println("Extra day 2 ");
+//        }
+//        else if(this.extraDays > 5){
+//            total += 10.00;
+//            System.out.println("Extra day 3 ");
+//        }
+//        else if(this.extraDays > 7){
+//            total += 20.00;
+//            System.out.println("Extra day 4 ");
+//        }
+
         if(this.extraDays > 7){
             total += 20.00;
             System.out.println("Extra day 4 ");
+        }
+        else if(this.extraDays > 5){
+            total += 10.00;
+            System.out.println("Extra day 3 ");
+        }
+        else if(this.extraDays > 3){
+            total += 5.00;
+            System.out.println("Extra day 2 ");
+        }
+        else if(this.extraDays > 1){
+            total += 1.00;
+            System.out.println("Extra day 1 ");
         }
         
         return total;
--- src/Main.java
+++ src/Main.java
@@ -12,30 +12,34 @@ public class Main {
         //Ingresando el dia de salida del vehiculo
         System.out.print("Ingrese la fecha de salida del vehiculo en formato YYYY-MM-DD: ");
         String exitDate = reader.nextLine();
-        rentalController.exitDate = LocalDate.parse(exitDate);
+        //CAMBIO
+        rentalController.setExitDate(exitDate);
 
         System.out.print("Ingrese la fecha estimada a regresar el vehiculo en formato YYYY-MM-DD: ");
         String estimateDate = reader.nextLine();
-        rentalController.estimateReturnDate = LocalDate.parse(estimateDate);
+        //CAMBIO
+        rentalController.setEstimateReturnDate(estimateDate);
 
         //Funcion para calcular el tiempo total
         rentalController.calculateTotalDays();
 
         //Imprimir un mensaje el numero de veces que se presto el vehiculo
-        if(rentalController.totalDays > 0){
-            System.out.println("Dia 1 de " + rentalController.totalDays);
+        if(rentalController.getTotalDays() > 0){
+            for (int i = 1; i <= rentalController.getTotalDays(); i++) {
+                System.out.println("Dia " + i + " de " + rentalController.getTotalDays() + " = " + (rentalController.getExitDay().plusDays(i)));
+            }
         }
 
         //Ingresando el dia real de regreso del vehiculo
         System.out.print("Ingrese la fecha real en la que regreso el vehiculo en formato YYYY-MM-DD: ");
         String realDate = reader.nextLine();
-        rentalController.returnDate = LocalDate.parse(realDate);
+        rentalController.setReturnDate(realDate);
 
         //Funcion para calcular los dias de retraso
         rentalController.calculateExtraDays();
 
         //Funcion para calcular el importe extra por los dias de retraso
-        double pricePerDay = 1.50 * rentalController.totalDays;
+        double pricePerDay = 1.50 * rentalController.getTotalDays();
         double billingTotal = rentalController.calculateBillingTotal(pricePerDay);
         System.out.println("El total por el alquiler del vehiculo es " + billingTotal);
     }
